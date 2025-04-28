ad5d2221cb2145a8eaff68c479f4ecbc419181e6
UPrime <UPrime@PRIME>
2025-04-28 20:42:02 +0300

    d

--- Entities/App/Common/GetRequest.cs
+++ Entities/App/Common/GetRequest.cs
@@ -2,9 +2,17 @@
 {
     public class GetRequest
     {
-        public int Skip { get; set; }
-        public int Take { get; set; }
+        public int? Skip { get; set; } = 0;
+        public int? Take { get; set; } = 100;
         public DateTime? From { get; set; }
         public DateTime? To { get; set; }
+        public OrderEnum? Order { get; set; } = OrderEnum.Asc;
+
+    }
+
+
+    public enum OrderEnum
+    {
+        Asc, Desc
     }
 }
